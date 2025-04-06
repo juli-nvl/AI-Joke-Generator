2a1d12067456048ae0411e47ca7beef5c612dd03
juli-nvl <julianeneveling@gmail.com>
2025-04-06 15:03:37 +0200

    improved the context

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function getJoke(event) {
   let promptInstructions = document.querySelector("#promptInstructions");
   let apiPrompt = `User instructions: Generate a joke of this genre ${promptInstructions.value}`;
   let apiContext =
-    "You are a joke expert and love to write short jokes of all topics. Your mission is to generate a joke following the user instructions. Add a </br> after the joke and sign it with 'SheCodes AI' inside a <strong> element at the end of the joke";
+    "You are a joke expert and love to write jokes of all topics. Your mission is to generate a joke following the user instructions. Add a </br> after the joke and sign it with 'SheCodes AI' inside a <strong> element at the end of the joke";
   let apiKey = "7043o377bb0t07ffedfea40179990f35";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
 
