549b7b32427280f52f97df40cc404b7f4da8941f
juli-nvl <julianeneveling@gmail.com>
2025-04-06 14:56:18 +0200

    added the AI-API to js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-function generateJoke(event) {
-  event.preventDefault();
+function generateJoke(response) {
   new Typewriter("#joke", {
-    strings: "The joke will appear here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
   });
 }
+function getJoke(event) {
+  event.preventDefault();
+  let promptInstructions = document.querySelector("#promptInstructions");
+  let apiPrompt = `User instructions: Generate a joke of this genre ${promptInstructions.value}`;
+  let apiContext =
+    "You are a joke expert and love to write short jokes of all topics. Your mission is to generate a joke following the user instructions. Add a </br> after the joke and sign it with 'SheCodes AI' inside a <strong> element at the end of the joke";
+  let apiKey = "7043o377bb0t07ffedfea40179990f35";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${apiPrompt}&context=${apiContext}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(generateJoke);
+}
 
 let jokeFormElement = document.querySelector("#joke-input");
-jokeFormElement.addEventListener("submit", generateJoke);
+jokeFormElement.addEventListener("submit", getJoke);
